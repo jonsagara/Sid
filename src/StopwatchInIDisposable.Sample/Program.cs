f124cfcf116354bce5bd7dc02489e4ea3cf4b036
Jon Sagara <jon@sagarasoftware.com>
2022-04-14 09:08:13 -0700

    Initial commit.

---
+++ src/StopwatchInIDisposable.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StopwatchInIDisposable;
+
+Console.WriteLine("Starting test of Sid...");
+
+using var totalTime = new Sid("total");
+
+using (var fakeDelay = new Sid("fake delay"))
+{
+    await Task.Delay(millisecondsDelay: 3_000);
+}
+
+Console.WriteLine("Done.");
---
+++ src/StopwatchInIDisposable/Sid.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace StopwatchInIDisposable;
+
+/// <summary>
+/// Stopwatch wrapped in IDisposable. Slightly simplifies the use of Stopwatch by not
+/// requiring you to explicitly write a try/finally block.
+/// </summary>
+public class Sid : IDisposable
+{
+    private static readonly Action<string, TimeSpan> _defaultOutput =
+        (tag, elapsed) => Console.WriteLine($"'{tag}' took {elapsed}.");
+    
+    private readonly string _tag;
+    private readonly Action<string, TimeSpan> _output;
+    private readonly Stopwatch _stopwatch;
+
+    /// <summary>
+    /// .ctor
+    /// </summary>
+    /// <param name="tag">A user-friendly name for the stopwatch.</param>
+    /// <param name="output">Takes the tag and the elapsed TimeSpan as arguments and outputs a string
+    /// after stopping the Stopwatch in the Dispose method.</param>
+    public Sid(string tag, Action<string, TimeSpan>? output = null)
+    {
+        ArgumentNullException.ThrowIfNull(tag);
+
+        _tag = tag;
+        _output = output ?? _defaultOutput;
+        _stopwatch = Stopwatch.StartNew();
+    }
+    
+    public void Dispose()
+    {
+        _stopwatch.Stop();
+        _output(_tag, _stopwatch.Elapsed);
+    }
+}
